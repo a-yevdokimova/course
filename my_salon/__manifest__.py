c6f4dc781ef828cea58676591af645ce5649ab26
a-yevdokimova <test@test.com>
2024-04-07 16:32:42 +0300

    [ADD] (course) my_salon: wizard

--- my_salon/__manifest__.py
+++ my_salon/__manifest__.py
@@ -12,25 +12,26 @@
     'data': [
         'security/ir.model.access.csv',
         'data/sequence.xml',
-        'wizard/change_master_wizard_views.xml',
-        'views/menu_views.xml',
         # 'wizard/change_master_wizard_views.xml',
+        'views/menu_views.xml',
+        'wizard/change_master_wizard_views.xml',
         'views/client_views.xml',
         'views/master_views.xml',
         'views/service_views.xml',
         'views/appointment_views.xml',
         'views/product_views.xml',
-        'views/provider_views.xml'
+        'views/provider_views.xml',
 
     ],
 
     'demo': [
-        'demo/client_demo.xml',
-        'demo/master_demo.xml',
         'demo/provider_demo.xml',
         'demo/product_demo.xml',
+        'demo/master_demo.xml',
+        'demo/client_demo.xml',
         'demo/service_demo.xml',
         'demo/appointment_demo.xml',
+
     ],
 
     'installable': True,
--- my_salon/wizard/change_master_wizard.py
+++ my_salon/wizard/change_master_wizard.py
@@ -8,14 +8,7 @@ class ChangeMasterWizard(models.TransientModel):
     client_id = fields.Many2one('client')
     master_id = fields.Many2one('master', string='New Master')
 
-    # def action_open_wizard(self):
-    #     return {
-    #         'name': _('Change master wizard'),
-    #         'type': 'ir.actions.act_window',
-    #         'view_mode': 'form',
-    #         'res_model': 'change.master.wizard',
-    #         'target': 'new',
-    #     }
-
     def action_change(self):
-        return
+        self.ensure_one()
+        self.client_id.master_id = self.master_id.id
+
